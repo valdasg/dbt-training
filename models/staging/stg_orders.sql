9167f60a480671ba99639e7547369eba5c1e75fb
valdas@tekto.lt <valdas@tekto.lt>
2022-05-18 08:56:16 +0000

    added delivery team seed to stg_orders

--- models/staging/stg_orders.sql
+++ models/staging/stg_orders.sql
@@ -1,5 +1,6 @@
 select 
 -- from raw orders 
+{{ dbt_utils.surrogate_key(['o.ORDEREID', 'c.customerid', 'p.productid']) }} as sk_orders,
 o.ordereid,
 o.orderdate,
 o.shipdate,
@@ -17,11 +18,16 @@ p.productid,
 p.category,
 p.productname,
 p.subcategory,
-{{ markup('ordersellingprice', 'ordercostprice') }} as markup
+{{ markup('ordersellingprice', 'ordercostprice') }} as markup,
+-- from seed delivery team
+d.delivery_team
 from {{ ref('raw_orders') }} as o
 left join {{ ref('raw_customer') }} as c
 on o.customerid = c.customerid
 left join {{ ref('raw_product') }} as p
 on o.productid = p.productid
+left join {{ ref('delivery_team') }} as d
+on 
+o.shipmode = d.shipmode
 
 --{{limit_data_in_dev('orderdate')}}
